187d0b137c95340e96a7a2102bf9138e6f3bc28f
Alexey Larionov <alxs.larionov@gmail.com>
2023-10-16 22:04:13 +0300

    Basic ImGUI and OpenGL 3.3

--- Makefile
+++ Makefile
@@ -4,7 +4,6 @@ CXX := ./ccache clang++
 SRC_ROOT ?= ./src
 OBJ_ROOT ?= $(BUILD_DIR)/$(ENGINE_CORE_SRC_ROOT)
 EXE_NAME ?= SlugReimplementation.exe
-OBJ_EXTENSION ?= object
 
 # NOTE: -Wpadded reports bloating of structs with padding !!
 CFLAGS = $(if $(DEBUG),-O0 -g, -O2) -std=c++17 -fno-exceptions -fno-rtti \
@@ -31,29 +30,47 @@ CFLAGS = $(if $(DEBUG),-O0 -g, -O2) -std=c++17 -fno-exceptions -fno-rtti \
 	-Wno-error=padded
 
 LDFLAGS = -lglfw -lGL -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
-INCLUDES = -I./vendor/glm -I./vendor/glad/include
+INCLUDES = -I./vendor/glm -I./vendor/glad/include -I./vendor/imgui
 
 
 SRCS := main.cpp
-OBJS := $(addprefix $(OBJ_ROOT)/, $(SRCS:.cpp=.$(OBJ_EXTENSION)))
+OBJS := $(addprefix $(OBJ_ROOT)/, $(SRCS:.cpp=.o))
 SRCS := $(addprefix $(SRC_ROOT)/, $(SRCS))
 # -- .cpp from source dir -> .o object files in build dir
-$(OBJS): $(OBJ_ROOT)/%.$(OBJ_EXTENSION): $(SRC_ROOT)/%.cpp
+$(OBJS): $(OBJ_ROOT)/%.o: $(SRC_ROOT)/%.cpp
 	mkdir -p $(OBJ_ROOT)
 	$(CXX) $(CFLAGS) -c $< -o $@ $(INCLUDES)
 
-$(OBJ_ROOT)/vendor/glad/gl.$(OBJ_EXTENSION): vendor/glad/src/gl.c
-	mkdir -p $(OBJ_ROOT)/vendor/glad
+VENDOR_OBJS=\
+	$(OBJ_ROOT)/vendor/glad/gl.o \
+	$(OBJ_ROOT)/vendor/imgui/imgui.o \
+	$(OBJ_ROOT)/vendor/imgui/imgui.o \
+	$(OBJ_ROOT)/vendor/imgui/imgui_impl_opengl3.o \
+	$(OBJ_ROOT)/vendor/imgui/imgui_impl_glfw.o \
+	$(OBJ_ROOT)/vendor/imgui/imgui_draw.o \
+	$(OBJ_ROOT)/vendor/imgui/imgui_widgets.o \
+	$(OBJ_ROOT)/vendor/imgui/imgui_tables.o \
+
+$(OBJ_ROOT)/vendor/glad/gl.o: vendor/glad/src/gl.c
+$(OBJ_ROOT)/vendor/imgui/imgui.o: vendor/imgui/imgui.cpp
+$(OBJ_ROOT)/vendor/imgui/imgui_impl_opengl3.o: vendor/imgui/backends/imgui_impl_opengl3.cpp
+$(OBJ_ROOT)/vendor/imgui/imgui_impl_glfw.o: vendor/imgui/backends/imgui_impl_glfw.cpp
+$(OBJ_ROOT)/vendor/imgui/imgui_draw.o: vendor/imgui/imgui_draw.cpp
+$(OBJ_ROOT)/vendor/imgui/imgui_widgets.o: vendor/imgui/imgui_widgets.cpp
+$(OBJ_ROOT)/vendor/imgui/imgui_tables.o: vendor/imgui/imgui_tables.cpp
+
+$(VENDOR_OBJS):
+	mkdir -p $(dir $@)
 	$(CXX) $(CFLAGS) -c $< -o $@ $(INCLUDES) -Wno-everything
 
 # -- .o from build dir -> executable in build dir
-$(BUILD_DIR)/$(EXE_NAME): $(OBJS) $(OBJ_ROOT)/vendor/glad/gl.$(OBJ_EXTENSION)
+$(BUILD_DIR)/$(EXE_NAME): $(VENDOR_OBJS) $(OBJS)
 	$(CXX) $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
 # vendor lib :: glm
 VENDOR_GLM_SRC := ./vendor/glm/glm/detail/glm.cpp
-VENDOR_GLM_OBJ := $(BUILD_DIR)/vendor/glm/glm.$(OBJ_EXTENSION)
-$(VENDOR_GLM_OBJ): $(BUILD_DIR)/vendor/glm/%.$(OBJ_EXTENSION): ./vendor/glm/glm/detail/%.cpp
+VENDOR_GLM_OBJ := $(BUILD_DIR)/vendor/glm/glm.o
+$(VENDOR_GLM_OBJ): $(BUILD_DIR)/vendor/glm/%.o: ./vendor/glm/glm/detail/%.cpp
 	mkdir -p $(BUILD_DIR)/vendor/glm
 	$(CXX) $(CFLAGS) -Wno-everything -fPIC -c $< -o $@ -I./vendor/glm
 $(BUILD_DIR)/vendor/glm.a: $(VENDOR_GLM_OBJ)
@@ -74,9 +91,9 @@ run: build
 
 .PHONY: rm
 rm:
-	rm -rf $(BUILD_DIR)/$(subst .,*.,$(EXE_NAME))
-	rm -rf $(BUILD_DIR)/**/*.$(OBJ_EXTENSION)
-	find $(BUILD_DIR) -name '*.$(OBJ_EXTENSION)' -delete
+	rm -f $(BUILD_DIR)/$(subst .,*.,$(EXE_NAME))
+	rm -f $(BUILD_DIR)/**/*.o
+	find $(BUILD_DIR) -name '*.o' -delete
 
 
 .PHONY: install_linux
@@ -89,10 +106,14 @@ install_linux: vendor/glm
 		&& cp ccache_prebuilt/*/ccache . \
 		&& rm -rf ccache_prebuilt
 
+vendor/imgui: 
+	wget https://github.com/ocornut/imgui/archive/refs/tags/v1.89.9.zip \
+		-O /tmp/imgui.zip && mkdir -p ./vendor && unzip /tmp/imgui.zip -d ./vendor/
+	mv ./vendor/imgui-1.89.9 ./vendor/imgui
+
 
 vendor/glm:
 #git submodule add -b tags/0.9.9.8 https://github.com/g-truc/glm.git vendor/glm
-	mkdir -p $(BUILD_DIR)/vendor/glm
 	wget https://github.com/g-truc/glm/releases/download/0.9.9.7/glm-0.9.9.7.zip \
 		-O /tmp/glm.zip && mkdir -p ./vendor && unzip /tmp/glm.zip -d ./vendor/
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,15 @@
 #include <glm/ext.hpp>
 #pragma clang diagnostic pop
 
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
 #include <glad/gl.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
  
 #include <stdlib.h>
-
 #include <iostream>
  
 struct VertexData
@@ -27,11 +30,11 @@ static const VertexData vertices[3] =
 };
  
 static const char* vertex_shader_text =
-"#version 110\n"
+"#version 330\n"
 "uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
+"in vec3 vCol;\n"
+"in vec2 vPos;\n"
+"out vec3 color;\n"
 "void main()\n"
 "{\n"
 "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
@@ -39,11 +42,12 @@ static const char* vertex_shader_text =
 "}\n";
  
 static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
+"#version 330\n"
+"in vec3 color;\n"
+"out vec4 frag;\n"
 "void main()\n"
 "{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
+"    frag = vec4(color, 1.0);\n"
 "}\n";
  
 static void error_callback(int error, const char* description)
@@ -63,17 +67,19 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 int main(void)
 {
     GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+    GLuint vao, vertex_buffer, vertex_shader, fragment_shader, program;
     GLint mvp_location, vpos_location, vcol_location;
- 
-    glfwSetErrorCallback(error_callback);
+    GLint success;
+    char infoLog[512];
  
     if (!glfwInit())
         exit(EXIT_FAILURE);
  
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
- 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+
     window = glfwCreateWindow(640, 480, "Simple example", nullptr, nullptr);
     if (!window)
     {
@@ -84,11 +90,29 @@ int main(void)
     glfwSetKeyCallback(window, key_callback);
  
     glfwMakeContextCurrent(window);
-    gladLoadGL(glfwGetProcAddress);
+        glfwSetErrorCallback(error_callback);
+    int versionGl = gladLoadGL(glfwGetProcAddress);
+    std::cout << "Loaded OpenGL " << GLAD_VERSION_MAJOR(versionGl) << '.' << GLAD_VERSION_MINOR(versionGl) << '\n';
+ 
     glfwSwapInterval(1);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
  
     // NOTE: OpenGL error checks have been omitted for brevity
- 
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
@@ -96,15 +120,39 @@ int main(void)
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, nullptr);
     glCompileShader(vertex_shader);
+
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertex_shader, sizeof(infoLog), nullptr, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        exit(EXIT_FAILURE);
+    }
  
     fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragment_shader, 1, &fragment_shader_text, nullptr);
     glCompileShader(fragment_shader);
+
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragment_shader, sizeof(infoLog), nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        exit(EXIT_FAILURE);
+    }
  
     program = glCreateProgram();
     glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
     glLinkProgram(program);
+
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(program, sizeof(infoLog), nullptr, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        exit(EXIT_FAILURE);
+    }
  
     mvp_location = glGetUniformLocation(program, "MVP");
     vpos_location = glGetAttribLocation(program, "vPos");
@@ -119,8 +167,12 @@ int main(void)
     glVertexAttribPointer(vcol_location_unsigned, 3, GL_FLOAT, GL_FALSE,
                           sizeof(vertices[0]), reinterpret_cast<const void*>(sizeof(float) * 2));
  
+    glm::vec4 clear_color (0.45f, 0.55f, 0.60f, 1.00f);
+    float rotation_radians;
     while (!glfwWindowShouldClose(window))
     {
+        glfwPollEvents();
+
         float ratio;
         int width, height;
  
@@ -128,21 +180,50 @@ int main(void)
         ratio = static_cast<float>(width) / static_cast<float>(height);
  
         glViewport(0, 0, width, height);
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            static int counter;
+            ImGui::Begin("Hello, world!");
+            ImGui::Text("This is some useful text.");
+            ImGui::SliderFloat("rotation", &rotation_radians, 0.0f, 2.f * glm::pi<float>());
+            ImGui::ColorEdit3("clear color", glm::value_ptr(clear_color));
+            if (ImGui::Button("Button"))
+                counter++;
+            ImGui::End();
+        }
+
+        glClearColor(clear_color.x * clear_color.w,
+                     clear_color.y * clear_color.w,
+                     clear_color.z * clear_color.w,
+                     clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
- 
+
         glm::mat4x4 m(1.0f);
-        m = glm::rotate(m, static_cast<float>(glfwGetTime()), glm::vec3(0.f, 0.f, 1.f));
+        m = glm::rotate(m, rotation_radians, glm::vec3(0.f, 0.f, 1.f));
         glm::mat4 p = glm::ortho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         glm::mat4 mvp = p * m;
  
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
         glDrawArrays(GL_TRIANGLES, 0, 3);
- 
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
-        glfwPollEvents();
+        
     }
  
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
     glfwDestroyWindow(window);
  
     glfwTerminate();
